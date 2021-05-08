3febdc5be2203a87a2e06b514c3a7eb631d8f429
宜桐 李 <2218868731@qq.com>
2021-05-08 23:52:23 +0800

    修改后的代码

--- test_5_8/test.c
+++ test_5_8/test.c
@@ -21,7 +21,7 @@ int main()
 
 	return 0;
 }
-#endif
+
 
 
 
@@ -52,8 +52,85 @@ void print2(int* arr, int length)//传进来的arr其实是首元素的地址
 int main()
 {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int(*parr)[10] = &arr;
+	int i=0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", (*parr)[i]);
+	}
+	printf("\n");
+	for ( i = 0; i < 10; i++)
+	{
+		printf("%d ", *(*parr + i));
+	}
+	printf("\n");
 	print1(arr, 10);
 	print2(arr, 10);
 	return 0;
 }
 
+#endif
+
+
+//二维数组的数组指针
+
+//常规打印二维数组方法
+void print(int arr[3][5], int x, int y) 
+{
+	int i = 0;
+	int j = 0;
+	for ( i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+//使用数组指针打印
+void print2(int(*p)[5], int x, int y)// p 是第一行的地址
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", *(*(p+i)+j));
+		}
+		printf("\n");
+	}
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ",(*(p+i))[j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5,},{2,3,4,5,6},{3,4,5,6,7} };
+	print(arr, 3, 5);//arr 数组名 就是首元素的地址
+	print2(arr, 3, 5);//数组名可以是第一列的地址
+
+	int i = 0;
+	int arr2[5] = { 1,2,3,4,5 };
+	int* p = arr2;
+	for ( i = 0; i <5; i++)
+	{
+		//以下四种形式等价：
+		printf("%d ", arr2[i]);
+		printf("%d ", *(p+i));
+		printf("%d ", p[i]);
+		printf("%d ", *(arr2+i));
+	}
+	printf("\n");
+	return 0;
+}
