e77788c45cea76191d1e86c1eb5a36a8a89b1a3f
宜桐 李 <2218868731@qq.com>
2021-06-03 16:58:53 +0800

    6月3日更新

--- test_5_11/练习题.c
+++ test_5_11/练习题.c
@@ -191,22 +191,220 @@ int main()
 // ***
 //  *
 //
-#endif
+
+
+
+//饮料问题，一元一瓶，俩空瓶换一瓶饮料
+int main()
+{
+	int money = 20;    //花多少钱
+	int bottle = money;//空瓶数
+	int count = money; //喝掉的瓶数
+	while (bottle > 1)
+	{
+		count += bottle / 2;
+		bottle = bottle / 2 + bottle % 2;
+	}
+	printf("%d\n", count);
+	return 0;
+}
+
+
+//5人参加跳水比赛，每个人说对了一半，确定比赛名次
+int main()
+{
+	for (size_t A = 1; A <= 5; A++)
+	{
+		for (size_t B = 1; B <= 5; B++)
+		{
+			for (size_t C = 1; C <= 5; C++)
+			{
+				for (size_t D = 1; D <= 5; D++)
+				{
+					for (size_t E = 1; E <= 5; E++)
+					{
+						if ((((B==2)+(A==3)==1) &&
+							((B==2)+(E==4)==1) &&
+							((C==1)+(D==2)==1) &&
+							((C==5)+(D==3)==1) &&
+							((E==4)+(A==1)==1))&&
+							(A * B * C * D * E == 120))
+						{
+							printf("A=%d B=%d C=%d D=%d E=%d\n", A, B, C, D, E);
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+//旋转字符串函数，左旋k个字符
+//ABCD --> BCDA
+//ABCD --> CDAB
+
+//暴力求解法
+void Rot_Left(char* arr0, int n)
+{
+	assert(arr0 != NULL);
+	int len = strlen(arr0);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < n; i++)
+	{
+		//翻转一个字符
+		char tmp = *arr0;
+		for(j=0; j< len-1; j++)
+		{
+			*(arr0 + j) = *(arr0 + 1 + j);
+		}
+		*(arr0-1+len) = tmp;
+	}
+}
+
+//逆序求解发（三步反转法）
+void reverse(char* arr, int n)
+{
+	int left = 0;
+	int right = n-1;
+	while (left < right)
+	{
+		char tmp = *(arr + left);
+		*(arr + left) = *(arr + right);
+		*(arr + right) = tmp;
+		left++;
+		right--;
+	}
+}
+void Rot_Left2(char* arr, int n)
+{
+	assert(arr);//检验是否为空指针
+	int len = strlen(arr);
+	assert(n <= len);
+	reverse(arr, n);//逆序左边
+	reverse(arr + n, len-n);//逆序右边
+	reverse(arr, len);//逆序整体
+}
+int main()
+{
+	char arr[] = "abc0";
+	printf("%s\n", arr);
+	int n = 4;
+	//Rot_Left(arr, n);
+	Rot_Left2(arr, n);
+	printf("%s\n", arr);
+	return 0;
+}
+
+
+//普通的查找函数  找到返回 1 ，找不到返回 0 
+int Findnum_1(int arr[3][3], int k, int row, int col)
+{
+	int x = 0;
+	int y = col - 1;
+	while (x < row && y >= 0)
+	{
+		if (arr[x][y] > k)
+			y--;
+		else if (arr[x][y] < k)
+			x++;
+		else
+			return 1;//相等的时候 返回 1
+	}
+	return 0;//找不到
+}
+
+//进阶的查找函数  找到返回 下标  找不到返回 0 
+int Findnum_2(int arr[3][3], int k, int* row, int* col)
+{
+	int x = 0;
+	int y = *col - 1;
+	while (x < *row && y >= 0)
+	{
+		if (arr[x][y] > k)
+			y--;
+		else if (arr[x][y] < k)
+			x++;
+		else
+		{
+			*row = x;
+			*col = y;
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[3][3]={ {1,2,3},
+					{4,5,6},
+					{7,8,9} };//这是一个杨氏数组
+	int k = 5;
+	int ret = 0;
+	int x = 3;
+	int y = 3;
+
+	ret = Findnum_1(arr,k,3,3);//查找函数，找到返回 1，找不到返回 0.
+	if (ret == 1)
+		printf("FIND\n");
+	else
+		printf("NOT FOUND\n");
+
+	ret = Findnum_2(arr, k, &x, &y);//找到了返回 下标，找不到返回 0.
+	if (ret == 1)
+		printf("(%d %d)\n", x, y);
+	else
+		printf("NOT FOUND\n");
+	return 0;
+}
+
+//这是自己实现 strlen
+//三种实现方法 1计数器  2函数递归  3指针相减
+int my_strlen(const char* arr)
+{
+	assert(arr != NULL);
+	int count = 0;
+	while(*arr!='\0')
+	{
+		arr++;
+		count++;
+	}
+	return count;
+}
 
 int main()
 {
-	int n = 20;
-	int sum = n;
-	int count = 0;
-	while (n / 2)
+	char arr[] = "abcdef";
+	int count = my_strlen(arr);
+	printf("%d\n", count);
+	return 0;
+}
+#endif
+
+
+//自己实现 strcpy 
+char* my_strcpy(char* arr1, const char* arr2)
+{
+	assert(arr1 != NULL);
+	assert(arr2 != NULL);
+	char* ret = arr1;
+	//拷贝arr2到arr1里面 包括'\0'
+	while (*arr1++ = *arr2++)
 	{
-		n /= 10;
-		sum += n;
-		if (n % 10)
-			count++;
+		;
 	}
-	n = count;
-	if(count)
-	printf("%d\n", sum);
+	//返回目的函数的首地址
+	return ret;
+}
+int main()
+{
+	char arr1[] = "abcde";
+	char arr2[] = "134";
+
+	char* ret = my_strcpy(arr1, arr2);//把 arr2 复制到 arr1 里面去
+	printf("%s\n", ret);
 	return 0;
 }
